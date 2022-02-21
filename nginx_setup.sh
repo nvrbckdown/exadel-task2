8877e27753bdb1e1b5c48285755aa3c2162ee179
Abdulkodir Yusupov <44323682+nvrbckdown@users.noreply.github.com>
2022-02-21 11:08:44 +0500

    Add bash script and nginx conf file

---
+++ nginx_setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+URL=DOMAIN_NAME
+
+cd
+git clone https://github.com/nvrbckdown/exadel-task2.git
+
+cd exadel-task2
+apt update && apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
+
+apt install python3-venv
+python3 -m venv venv
+source venv/bin/activate
+
+pip install -r req.txt
+pip install wheel uwsgi
+
+deactivate
+
+cp app.service /etc/systemd/system/
+
+cp hello-page.nginx.conf /etc/nginx/sites-available/$URL
+ln -s /etc/nginx/sites-available/$URL /etc/nginx/sites-enabled/
+
+systemctl start app.service
